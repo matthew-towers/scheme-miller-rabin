a294de297bf8cf34a9ca028a650d5a1ec21b448d
Matthew Towers <matthew.towers@gmail.com>
2020-12-21 11:19:57 +0000

    eliminate double call

--- miller_rabin.scm
+++ miller_rabin.scm
@@ -1,5 +1,7 @@
+#lang racket
 ; factor as many 2s out of n as possible:
 ; input n, output (k, e) where n=k2^e with k odd
+
 (define (divide-out-powers-of-2 n)
   (define (DOP2 n e)
     (if (= (remainder n 2) 1)
@@ -23,9 +25,9 @@
 ; ...
 ; a^{2^{e-1}} \equiv -1 \mod n
 ;... are false?
-(define (witness? a e k n)
-  (let ((x (expmod a k n))) ; x= a^k mod n
-    (and (not (= x 1)) (witness-test-iter x e n))))
+(define (witness? a ke n)
+  (let ((x (expmod a (car ke) n))) ; x= a^k mod n
+    (and (not (= x 1)) (witness-test-iter x (cdr ke) n))))
 
 (define (witness-test-iter x e n)
   (cond ((= x (- n 1)) #f) ; a congruence is true. x isn't a witness
@@ -34,10 +36,9 @@
     
 ; MR primality test, with numberOfTrials runs. n must be odd.
 (define (is-prime-MR? n trials)
-  (let ( (k (car (divide-out-powers-of-2 (- n 1))))
-	 (e (cdr (divide-out-powers-of-2 (- n 1)))) ); this is silly but it doesn't seem that we can let u be divideOutPowersOf2 (- n 1) and k be car of u in the same let line.
+  (let ((ke (divide-out-powers-of-2 (- n 1))))
     (define (is-prime-MR-iter trials)
       (cond ((= trials 0) #t)
-	    ((witness? (+ 1 (random (- n 2))) e k n) #f)
+	    ((witness? (+ 1 (random (- n 2))) ke n) #f)
 	    (else (is-prime-MR-iter (- trials 1)))))
     (is-prime-MR-iter trials) ))
